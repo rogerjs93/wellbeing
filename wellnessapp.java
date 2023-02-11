9f68e7d3ac5c1de6590d17a9eeb27737dcf753ac
Roger Salas <58913883+rogerjs93@users.noreply.github.com>
2023-02-11 16:34:58 +0200

    Create wellnessapp.java

---
+++ wellnessapp.java
@@ -0,0 +1,26 @@
+Class User:
+
+//Attributes
+private String username;
+private String password;
+private String name;
+private String age;
+private String gender;
+private String weight;
+private String height;
+private String language;
+
+//Constructor
+public User(String username, String password, String name, String age, String gender, String weight, String height, String language){
+this.username = username;
+this.password = password;
+this.name = name;
+this.age = age;
+this.gender = gender;
+this.weight = weight;
+this.height = height;
+this.language = language;
+}
+
+//Methods
+public
